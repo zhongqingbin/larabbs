5fb165160e4fba1af0e552836d00779dfb3f9563
qingbin <qingbin@mytopfun.com>
2020-07-02 18:06:58 +0800

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,26 +14,31 @@
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
+    @yield('styles')
+
 </head>
 
 <body>
-<div id="app" class="{{ route_class() }}-page">
+    <div id="app" class="{{ route_class() }}-page">
 
-    @include('layouts._header')
+        @include('layouts._header')
 
-    <div class="container">
+        <div class="container">
 
-        @include('shared._messages')
+            @include('shared._messages')
 
-        @yield('content')
+            @yield('content')
 
+        </div>
+
+        @include('layouts._footer')
     </div>
 
-    @include('layouts._footer')
-</div>
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
 
-<!-- Scripts -->
-<script src="{{ mix('js/app.js') }}"></script>
+    @yield('scripts')
+
 </body>
 
 </html>
