4367acfc93a92bee43dab6e1b983ecdec8e86945
qingbin <qingbin@mytopfun.com>
2020-07-21 18:41:38 +0800

    支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use App\Models\Reply;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -33,7 +33,7 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
@@ -44,9 +44,10 @@ class TopicReplied extends Notification
      */
     public function toMail($notifiable)
     {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
+                    ->line('你的话题有新回复！')
+                    ->action('查看回复',$url)
                     ->line('Thank you for using our application!');
     }
 
