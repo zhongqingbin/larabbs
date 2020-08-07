611a93f54d9ab09bde3b742083133be0de121b08
qingbin <qingbin@mytopfun.com>
2020-08-07 17:59:01 +0800

    删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -54,4 +54,12 @@ class Topic extends Model
     {
         return $this->hasMany(Reply::class);
     }
+
+    public function updateReplyCount()
+    {
+        $this->reply_count = $this->replies->count();
+        $this->save();
+    }
+
+
 }
--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -43,4 +43,9 @@ class TopicObserver
         }
     }
 
+    public function deleted(Topic $topic)
+    {
+        \DB::table('replies')->where('topic_id', $topic->id)->delete();
+    }
+
 }
